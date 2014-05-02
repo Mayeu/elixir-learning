b99784ebc0ccb1905639ef195142c6f963543b33
Mayeu <m@6x9.fr>
2014-05-02 19:31:51 +0200

    File the make_deck function

--- elixir_card_deck/lib/elixir_card_deck.ex
+++ elixir_card_deck/lib/elixir_card_deck.ex
@@ -1,5 +1,7 @@
 defmodule ElixirCardDeck do
   def make_deck do
-    []
+    values = [:a, 2, 3, 4, 5, 6, 7, 8, 9, 10, :j, :q, :k]
+    suits = [:spades, :clubs, :diamonds, :hearts]
+    for x <- values, y <- suits, do: {:card, x, y}
   end
 end
